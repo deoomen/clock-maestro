e7cec66572bd9231865cd93cd3c88cb751103be6
deoomen <deoomen@protonmail.com>
2021-04-06 14:57:51 +0200

    feat: :sparkles: add system clock

---
+++ src/Clock/SystemClockMaestro.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ClockMaestro\Clock;
+
+use ClockMaestro\ClockMaestro;
+use DateTimeImmutable;
+use DateTimeZone;
+
+class SystemClockMaestro implements ClockMaestro
+{
+    private DateTimeZone $timezone;
+
+    private function __construct(DateTimeZone $timezone)
+    {
+        $this->timezone = $timezone;
+    }
+
+    public static function fromTimezone(DateTimeZone $timezone): self
+    {
+        return new self($timezone);
+    }
+
+    public static function fromSystemTimezone(): self
+    {
+        return self::fromTimezone(new DateTimeZone(date_default_timezone_get()));
+    }
+
+    public static function fromUTC(): self
+    {
+        return self::fromTimezone(new DateTimeZone('UTC'));
+    }
+
+    public function now(): DateTimeImmutable
+    {
+        return new DateTimeImmutable('now', $this->timezone);
+    }
+}
---
+++ src/ClockMaestro.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ClockMaestro;
+
+use DateTimeImmutable;
+use DateTimeZone;
+
+interface ClockMaestro
+{
+    public static function fromTimezone(DateTimeZone $timezone): self;
+
+    public static function fromSystemTimezone(): self;
+
+    public static function fromUTC(): self;
+
+    public function now(): DateTimeImmutable;
+}
